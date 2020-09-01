05ce66144e0b1f2cb613926702a94102d536f04f
Jeff Alexander <jefalexa@cisco.com>
2020-09-01 09:41:04 -0600

    Added directory drill function to file picker class

--- jeff_udf.py
+++ jeff_udf.py
@@ -85,6 +85,13 @@ def local_find_to_df(working_dir, x=".*"):
         file_list = []
         return(pd.DataFrame(columns=['Filename', 'Size', 'Modified Time'], data=file_list))
  
+def local_find_dir(working_dir):
+	'''Returns a list of root directories in a given directory'''
+	directory_list = []
+	for name in os.listdir(working_dir):
+		if os.path.isdir(os.path.join(working_dir, name)):
+			directory_list.append(os.path.join(working_dir, name))
+		return(directory_list)
 
 
 def interactive_file_saveloc(dir_list, search_pattern):
@@ -104,56 +111,6 @@ def interactive_file_saveloc(dir_list, search_pattern):
             return(file_df)    
     interact(test01, dir_input=dir_list, search_pattern=fixed(search_pattern))
  
- 
-class file_picker():
-    '''
-    Create a file_picker object mypicker01, then call mypicker01.interactive_file_picker(dir_list=['./', '../'], search_pattern=".*") to pick files from a set of directories.  
-    Then reference file name, full file path and the directory as mypicker01.file_name, mypicker01.file_path, mypicker01.file_dir.  Or all three as mypicker01.interactive_file_picker_output
-    '''
-    dir_list = []
-    search_pattern = ""
-    def interactive_file_picker(self, dir_list=['./', '../'], search_pattern=".*"):
-        file = ""
-        interactive_file_picker_output = []
-        file_name = ""
-        file_path = ""
-        file_dir = ""
-        def ifp_sub01(dir_input, search_pattern):
-            file_list = self.local_find_to_df(dir_input, search_pattern).sort_values(by='Modified Time', ascending=False)['Filename'].tolist()
-            file_list.insert(0, "")
-            interact(ifp_sub02, dir_input=fixed(dir_input), file_picker=file_list, search_pattern=fixed(search_pattern))
-        def ifp_sub02(dir_input, file_picker, search_pattern):
-            file = os.path.join(dir_input, file_picker)
-            nonlocal interactive_file_picker_output
-            nonlocal file_name
-            nonlocal file_path
-            nonlocal file_dir
-            if len(file_picker) > 0:
-                self.interactive_file_picker_output = [file_picker, file, dir_input]
-                self.file_name, self.file_path, self.file_dir = [file_picker, file, dir_input]
-                print("  File:  {}\n  Path:  {}\n  Size: {}\n  Modified: {}".format(file_picker, file, os.stat(file).st_size, dt.datetime.strftime(dt.datetime.fromtimestamp(os.stat(file).st_mtime), '%m-%d-%y %H:%M')))
-            else:
-                return(self.local_find_to_df(dir_input, search_pattern).sort_values(by='Modified Time', ascending=False))
-        interact(ifp_sub01, dir_input=dir_list, search_pattern=fixed(search_pattern))
-
-    def local_find_to_df(self, working_dir, x=".*"):
-        pattern = re.compile(x)
-        file_list = []
-        try:
-            for file in os.listdir(working_dir):
-                if re.match(pattern=pattern, string=file):
-                    f2 = os.path.join(working_dir, file)
-                    fsize = os.stat(f2).st_size
-                    fts = os.stat(f2).st_mtime
-                    fdt = dt.datetime.fromtimestamp(fts)
-                    #print(file, fsize, fdt)
-                    file_list.append([file, fsize, fdt])
-            return(pd.DataFrame(columns=['Filename', 'Size', 'Modified Time'], data=file_list))
-        except:
-            print("Error")
-            file_list = []
-            return(pd.DataFrame(columns=['Filename', 'Size', 'Modified Time'], data=file_list))
-
 
 
 def interactive_table_frame(df):
@@ -286,3 +243,125 @@ def read_myclip():
 	df = pd.read_csv(file, index_col=0)
 	return(df)
 	
+	
+class file_picker():
+    '''
+    Create a file_picker object mypicker01, then call mypicker01.interactive_file_picker(dir_list=['./', '../'], search_pattern=".*") to pick files from a set of directories.  
+    Then reference file name, full file path and the directory as mypicker01.file_name, mypicker01.file_path, mypicker01.file_dir.  Or all three as mypicker01.interactive_file_picker_output
+    '''
+    def __init__(self, dir_list=['./', '../'], search_pattern=".*"):
+        self.file = ""
+        self.interactive_file_picker_output = []
+        self.file_name = ""
+        self.file_path = ""
+        self.file_dir = ""
+        self.dir_list = []
+        self.search_pattern = ""
+        self.dir_list = dir_list
+        self.search_pattern = search_pattern
+    def select(self):
+        dir_list = self.dir_list
+        search_pattern = self.search_pattern
+        def ifp_sub01(dir_input, search_pattern):
+            file_list = self.__local_find_to_df(dir_input, search_pattern).sort_values(by='Modified Time', ascending=False)['Filename'].tolist()
+            file_list.insert(0, "")
+            interact(ifp_sub02, dir_input=fixed(dir_input), file_picker=file_list, search_pattern=fixed(search_pattern))
+        def ifp_sub02(dir_input, file_picker, search_pattern):
+            self.file = os.path.join(dir_input, file_picker)
+            if len(file_picker) > 0:
+                file_path = os.path.join(dir_input, self.file)
+                if os.path.isdir(file_path):
+                    print("'{}' added to directory list.  Reload select function.".format(file_path))
+                    self.dir_list.append(file_path)
+                else:
+                    self.interactive_file_picker_output = [file_picker, self.file, dir_input]
+                    self.file_name, self.file_path, self.file_dir = [file_picker, self.file, dir_input]
+                    print("  File:  {}\n  Path:  {}\n  Size: {}\n  Modified: {}".format(file_picker, self.file, os.stat(self.file).st_size, dt.datetime.strftime(dt.datetime.fromtimestamp(os.stat(self.file).st_mtime), '%m-%d-%y %H:%M')))
+            else:
+                return(self.__local_find_to_df(dir_input, search_pattern).sort_values(by='Modified Time', ascending=False))
+        interact(ifp_sub01, dir_input=dir_list, search_pattern=fixed(search_pattern))
+
+    def __local_find_to_df(self, working_dir, x=".*"):
+        pattern = re.compile(x)
+        file_list = []
+        try:
+            for file in os.listdir(working_dir):
+                if re.match(pattern=pattern, string=file):
+                    f2 = os.path.join(working_dir, file)
+                    fsize = os.stat(f2).st_size
+                    fts = os.stat(f2).st_mtime
+                    fdt = dt.datetime.fromtimestamp(fts)
+                    #print(file, fsize, fdt)
+                    file_list.append([file, fsize, fdt])
+            return(pd.DataFrame(columns=['Filename', 'Size', 'Modified Time'], data=file_list))
+        except:
+            print("Error")
+            file_list = []
+            return(pd.DataFrame(columns=['Filename', 'Size', 'Modified Time'], data=file_list))
+
+
+class interactive_tabs():
+    def __init__(self, df):
+        self.tab_contents = []
+        self.tab = widgets.Tab()
+        self.df = df
+    def select(self):
+        self.tab_contents = self.df.columns
+        children = []
+        for name in self.tab_contents:
+            try:
+                l1 = self.df[name].dropna().sort_values().unique().tolist()
+                l1.insert(0, '')
+                if self.df[name].dtype == (float or int):
+                    f1 = widgets.HBox([widgets.Label(name), widgets.FloatRangeSlider(value=[self.df[name].min(), self.df[name].max()], min=self.df[name].min(), max=self.df[name].max(), step=1, disabled=False, continuous_update=False, orientation='horizontal', readout=True, readout_format='.0f', ) ])
+                else:
+                    if len(l1) <= 20:
+                        f1 = widgets.HBox([widgets.Label(name), widgets.SelectMultiple(options=l1, disabled=False) ])
+                    else:
+                        f1 = widgets.HBox([widgets.Label(name), widgets.Text(value='.*',placeholder='.*',disabled=False) ])
+
+                children.append(f1)
+            except:
+                print("Error on {}".format(name))
+        self.tab.children = children
+        for i in range(len(children)):
+            self.tab.set_title(i, self.tab_contents[i])
+        return(self.tab)
+
+    def display(self):
+        index_num = 0
+        df1 = self.df
+        total_len = len(df1)
+        for index_num in range(0, len(self.tab_contents)):
+            tname = self.tab_contents[index_num]
+            tval = self.tab.children[index_num].children[1].value
+            if tval:
+                vt = type(tval)
+                if vt == type(tuple()):
+                    if df1[tname].dtype == (float or int):
+                        if ((self.tab.children[index_num].children[1].min == tval[0]) & (self.tab.children[index_num].children[1].max == tval[1])):
+                            df1 = df1
+                        else:
+                            f1 = df1[tname] >= tval[0]
+                            f2 = df1[tname] <= tval[1]
+                            df1 = df1.loc[f1&f2, :]
+                            print("____________\n{} Min: {} - Max: {}".format(tname, tval[0], tval[1]))
+                            print("Matched {} entries".format(len(df1)))
+                    else:
+                        if tval == ('',):
+                            continue
+                        else:
+                            f1 = df1[tname].isin(tval)
+                            df1 = df1.loc[f1, :]
+                        print("____________\n{} {}".format(tname, tval))
+                        print("Matched {} entries".format(len(df1)))
+                else:
+                    if tval == '.*':
+                        df1 = df1
+                    else:
+                        Match_Case = True
+                        df1 = df1.loc[df1[tname].apply(lambda x: check_match(x, tval, Match_Case)) == True]
+                        print("____________\n{}: '{}' Matched:\n".format(tname, tval), df1[tname].value_counts())
+
+        print("____________\n", "Matched {} of {} entries".format(len(df1), total_len))
+        return(df1)
